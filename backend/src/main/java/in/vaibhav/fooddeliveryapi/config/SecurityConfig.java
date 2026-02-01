21ccc3265ce39614a582f30a604560ce8e22b937
Vaibhav Rana <vaibhavranaa@gmail.com>
2026-02-01 21:05:49 +0530

    fixed issues

--- backend/src/main/java/in/vaibhav/fooddeliveryapi/config/SecurityConfig.java
+++ backend/src/main/java/in/vaibhav/fooddeliveryapi/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/register",
                                 "/api/login",
+                                "/api/foods",
                                 "/api/foods/**"
                         ).permitAll()
 
